9217590a8959241a530903373902d378b3313e78
deppravacion <deppravacion@gmail.com>
2022-09-27 21:35:52 -0600

    noodles

--- api3.js
+++ api3.js
@@ -1,8 +1,34 @@
-fetch(`https://freerandomapi.cyclic.app/api/v1/dogs`);
-  .then(res => {
-    return res.json();
-  })
-  .then(data => {
-    console.log(data);
-  })
-  .catch(err => console.log(err));
+
+const getData = async (paths) => {
+  const promises = paths
+    .map(path => fetch(`https://freerandomapi.cyclic.app/api/v1/${path}`)
+    .then(r => r.json()));
+
+  // const accounts = await Promise.all(promises);   
+  // console.log(accounts);
+  const accounts = await Promise.all(promises);
+  console.log(accounts);
+
+  [treatObj, dogObj] = accounts;
+
+  const myHtml = elemFromHtml(`
+  <h2>${dogObj.data[4].name}</h2>  
+  `);
+
+  const firstHtml = elemFromHtml(`
+  <h3>${treatObj.data[4].name}</h3>
+  `)
+  document.body.appendChild(firstHtml, myHtml);
+  
+  
+};
+getData(["desserts?category=Ice_Cream&limit=50", "dogs"]);
+
+{/* <h3>${treatObj.data[4].name}</h3> */}
+
+const elemFromHtml = (html) => {
+  const template = document.createElement("template");  
+  template.innerHTML = html.trim();
+  return template.content.firstElementChild;    
+}
+
