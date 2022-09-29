45aef3565caa85de4c6b17a8869168a17074e907
deppravacion <deppravacion@gmail.com>
2022-09-28 23:44:31 -0600

    add array and list

--- api3.js
+++ api3.js
@@ -10,21 +10,36 @@ const getData = async (paths) => {
   console.log(accounts);
 
   [treatObj, dogObj] = accounts;
+  let dogNamesArr = [];
+  let ul = document.createElement('ul');
+  dogObj.data.forEach(item => {
+    console.log(item.name);
+    dogNamesArr.push(item.name);
+ 
+  })
+
+  dogNamesArr.sort();
+  dogNamesArr.reverse();
+
+  for (let dog of dogNamesArr) {
+    let li = document.createElement('li');
+    li.innerText = dog;
+    ul.appendChild(li);
+  }
 
   const myHtml = elemFromHtml(`
   <h2>${dogObj.data[4].name}</h2>  
   `);
 
-  const firstHtml = elemFromHtml(`
-  <h3>${treatObj.data[4].name}</h3>
-  `)
-  document.body.appendChild(firstHtml, myHtml);
+  document.body.appendChild(ul);
   
   
 };
 getData(["desserts?category=Ice_Cream&limit=50", "dogs"]);
 
 {/* <h3>${treatObj.data[4].name}</h3> */}
+
+
 
 const elemFromHtml = (html) => {
   const template = document.createElement("template");  
